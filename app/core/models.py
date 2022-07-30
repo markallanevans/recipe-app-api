72ca899f465bd48668b7643e4587cdf486df465a
Mark Evans <markallanevans@gmail.com>
2022-07-30 15:31:34 +0700

    feat: normalize new user emails

--- app/core/models.py
+++ app/core/models.py
@@ -9,17 +9,19 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        
-        user = self.model(email=email, **extra_fields)
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
