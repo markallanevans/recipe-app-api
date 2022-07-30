e88ed8ee057f82a20fb7d7a22669223fb424f4cc
Mark Evans <markallanevans@gmail.com>
2022-07-30 16:04:08 +0700

    feat: validate new user emails

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,8 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating new user with out email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
